42400a6d14814ae08ea01e1c1e02373e1e1d1fbd
Andreas Börzel <andreas.boerzel@gmx.de>
2019-12-30 13:50:56 +0100

    feat: image normalization changed

--- license_plate_image_augmentator.py
+++ license_plate_image_augmentator.py
@@ -104,8 +104,7 @@ class LicensePlateImageAugmentator:
     @staticmethod
     def normalize_image(image):
         # normalize image data between 0 and 1
-        # image = (image - image.min()) / (image.max() - image.min())
-        image = image.astype(np.float32) / 255.
+        image = (image - image.min()) / (image.max() - image.min())
         return image
 
     def generate_plate_image(self, plate_img):
