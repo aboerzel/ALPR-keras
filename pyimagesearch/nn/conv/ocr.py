c070076f39439f6c4a1391b7cb23775f2c397166
Andreas Börzel <andreas.boerzel@gmx.de>
2020-01-04 12:25:21 +0100

    feat: CONV + BLSTM support

--- pyimagesearch/nn/conv/ocr.py
+++ pyimagesearch/nn/conv/ocr.py
@@ -43,6 +43,43 @@ class OCR:
         return input_data, output_data
 
     @staticmethod
+    def conv_blstm(input_shape, output_size):
+        conv_filters = 16
+        kernel_size = (3, 3)
+        pool_size = 2
+        time_dense_size = 32
+        rnn_size = 512
+
+        input_data = Input(name="input", shape=input_shape)
+
+        cnn = Conv2D(conv_filters, kernel_size, padding='same', kernel_initializer='he_normal')(input_data)
+        cnn = BatchNormalization()(cnn)
+        cnn = Activation('relu')(cnn)
+        cnn = MaxPooling2D(pool_size=(pool_size, pool_size))(cnn)
+
+        cnn = Conv2D(conv_filters, kernel_size, padding='same', kernel_initializer='he_normal')(cnn)
+        cnn = BatchNormalization()(cnn)
+        cnn = Activation('relu')(cnn)
+        cnn = MaxPooling2D(pool_size=(pool_size, pool_size))(cnn)
+
+        # CNN to RNN
+        shape = cnn.get_shape()
+        cnn = Reshape((shape[1], shape[2] * shape[3]))(cnn)
+        dense = Dense(time_dense_size, activation='relu', kernel_initializer='he_normal')(cnn)
+
+        # RNN layer
+        bgru = Bidirectional(LSTM(units=rnn_size, return_sequences=True), merge_mode="sum")(dense)
+        #bgru = BatchNormalization()(bgru)
+        bgru = Bidirectional(LSTM(units=rnn_size, return_sequences=True), merge_mode="concat")(bgru)
+        #bgru = BatchNormalization()(bgru)
+
+        # transforms RNN output to character activations:
+        dense = Dense(output_size, kernel_initializer='he_normal')(bgru)
+        output_data = Activation("softmax", name="output")(dense)
+
+        return input_data, output_data
+
+    @staticmethod
     def vgg_blstm(input_shape, output_size):
 
         input_data = Input(name='input', shape=input_shape, dtype='float32')  # (None, 128, 64, 1)
